d7891ee102a76f9be44d619bb683dc184d53259e
Tom Dessauvages <154484372+Teyva@users.noreply.github.com>
2025-01-22 23:10:00 -0500

    Update setup.bat

--- Deuxième version/setup.bat
+++ Deuxième version/setup.bat
@@ -19,8 +19,11 @@ if not defined new_password (
     goto password_prompt
 )
 
+REM Hash the password using PowerShell
+for /f "delims=" %%i in ('powershell -Command "[System.BitConverter]::ToString((New-Object Security.Cryptography.MD5CryptoServiceProvider).ComputeHash([System.Text.Encoding]::UTF8.GetBytes('%new_password%'))).Replace('-','').ToLower()"') do set "hashed_password=%%i"
+
 REM Use PowerShell to modify the JSON file
-powershell -Command "$json = Get-Content utilisateurs.json | ConvertFrom-Json; $admin = $json.utilisateurs | Where-Object { $_.role -eq 'admin' }; $admin.password = '%new_password%'; $json | ConvertTo-Json -Depth 10 | Set-Content utilisateurs.json"
+powershell -Command "$json = Get-Content utilisateurs.json | ConvertFrom-Json; $admin = $json.utilisateurs | Where-Object { $_.role -eq 'admin' }; $admin.password = '%hashed_password%'; $json | ConvertTo-Json -Depth 10 | Set-Content utilisateurs.json"
 
 echo Admin password set successfully in utilisateurs.json!
 
